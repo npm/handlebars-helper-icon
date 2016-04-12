b771509f836566bc5fc08a52e14b967395469be7
Aria Stewart <aredridel@dinhe.net>
2016-04-12 02:01:42 -0400

    Make tests system-independent

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ const handlebars = require('handlebars');
 const tap = require('tap');
 const icon = require('./');
 const ltx = require('ltx');
+const resolve = require('resolve');
+const murmurhash = require('murmurhash');
+
+const id = `symbol-${murmurhash.v3(resolve.sync("./test.svg"))}`;
 
 tap.test('register helper', function (t) {
 	handlebars.registerHelper('icon', icon);
@@ -11,29 +15,29 @@ tap.test('register helper', function (t) {
 
 tap.test('with attribute', function (t) {
 	const template = handlebars.compile(`{{icon "./test.svg" width="55"}}`);
-	t.equal(template({}), `<svg style="display: none"><symbol viewBox="0 0 2 2" id="symbol-2912310545"><circle r="1" x="1" y="1"/></symbol></svg>
+	t.equal(template({}), `<svg style="display: none"><symbol viewBox="0 0 2 2" id="${id}"><circle r="1" x="1" y="1"/></symbol></svg>
 <svg xmlns:xlink="http://www.w3.org/1999/xlink" width="55">
-    <use xlink:href="#symbol-2912310545"/>
+    <use xlink:href="#${id}"/>
   </svg>`);
 	t.end();
 });
 
 tap.test('without attribute', function (t) {
 	const template = handlebars.compile(`{{icon "./test.svg" }}`);
-	t.equal(template({}), `<svg style="display: none"><symbol viewBox="0 0 2 2" id="symbol-2912310545"><circle r="1" x="1" y="1"/></symbol></svg>
+	t.equal(template({}), `<svg style="display: none"><symbol viewBox="0 0 2 2" id="${id}"><circle r="1" x="1" y="1"/></symbol></svg>
 <svg xmlns:xlink="http://www.w3.org/1999/xlink">
-    <use xlink:href="#symbol-2912310545"/>
+    <use xlink:href="#${id}"/>
   </svg>`);
 	t.end();
 });
 
 tap.test('second render only uses', function (t) {
 	const template = handlebars.compile(`{{icon "./test.svg" }}{{icon "./test.svg" }}`);
-	t.equal(template({}), `<svg style="display: none"><symbol viewBox="0 0 2 2" id="symbol-2912310545"><circle r="1" x="1" y="1"/></symbol></svg>
+	t.equal(template({}), `<svg style="display: none"><symbol viewBox="0 0 2 2" id="${id}"><circle r="1" x="1" y="1"/></symbol></svg>
 <svg xmlns:xlink="http://www.w3.org/1999/xlink">
-    <use xlink:href="#symbol-2912310545"/>
+    <use xlink:href="#${id}"/>
   </svg><svg xmlns:xlink="http://www.w3.org/1999/xlink">
-    <use xlink:href="#symbol-2912310545"/>
+    <use xlink:href="#${id}"/>
   </svg>`);
 	t.end();
 });
